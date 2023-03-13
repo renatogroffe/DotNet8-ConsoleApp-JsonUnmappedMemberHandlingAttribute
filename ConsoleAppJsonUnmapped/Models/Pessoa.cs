d236e9f41a1d0fc1a026dec12e64428b33ec0c05
Renato Groffe <renato.groffe@yahoo.com.br>
2023-03-13 08:30:13 -0300

    .NET 8 + Console Application + Desserialização utilizando JsonUnmappedMemberHandlingAttribute
    .NET 8 + Console Application + Desserialização utilizando JsonUnmappedMemberHandlingAttribute

---
+++ ConsoleAppJsonUnmapped/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleAppJsonUnmapped.Models;
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
+public class Pessoa
+{
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+}
---
+++ ConsoleAppJsonUnmapped/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ConsoleAppJsonUnmapped.Models;
+
+Console.WriteLine("***** Testes com .NET 8 | JsonUnmappedMemberHandlingAttribute *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var dadosPessoas = new string[] { """{"Nome" : "Maria", "Peso" : 50.0, "Idade" : 27 }""",
+    """{"Nome" : "Pedro", "Idade" : 40 }""" };
+foreach (var dadosPessoa in dadosPessoas)
+{
+    try
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Valor a ser desserializado = {dadosPessoa}");
+        var pessoa = JsonSerializer.Deserialize<Pessoa>(dadosPessoa)!;
+        Console.WriteLine($"Valor da propriedade '{nameof(pessoa.Nome)}' = {pessoa.Nome}");
+        Console.WriteLine($"Valor da propriedade '{nameof(pessoa.Idade)}' = {pessoa.Idade}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu uma exceção do tipo '{ex.GetType().FullName}'");
+    }
+}
